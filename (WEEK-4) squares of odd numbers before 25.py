1a7805e2373fb539d688564671191a8efdb028c8
mkalla2-mech <mkalla2@student.gitam.edu>
2026-01-28 10:42:36 +0530

    Create (WEEK-4) squares of odd numbers before 25.py

---
+++ (WEEK-4) squares of odd numbers before 25.py
@@ -0,0 +1,22 @@
+for i in range(1, 100):
+     
+    if i == 25:
+        break
+    elif i % 2 != 0:
+        print(i * i)
+
+#OUTPUT
+1
+9
+25
+49
+81
+121
+169
+225
+289
+361
+441
+529
+
+=== Code Execution Successful ===
