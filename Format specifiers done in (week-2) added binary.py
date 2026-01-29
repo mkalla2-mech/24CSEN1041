b48f69d3bccb9046cb302a101734a64f9db9b519
mkalla2-mech <mkalla2@student.gitam.edu>
2026-01-29 14:54:09 +0530

    Create Format specifiers done in (week-2) added binary.py

---
+++ Format specifiers done in (week-2) added binary.py
@@ -0,0 +1,15 @@
+a = 15
+
+print("Decimal representation:", a)
+print("Binary representation:", bin(a))
+print("Octal representation:", oct(a))
+print("Hexadecimal representation:", hex(a))
+
+#OUTPUT
+
+Decimal representation: 15
+Binary representation: 0b1111
+Octal representation: 0o17
+Hexadecimal representation: 0xf
+
+=== Code Execution Successful ===
